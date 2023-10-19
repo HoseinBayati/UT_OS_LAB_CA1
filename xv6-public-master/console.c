5fe5168491937a39ebaeba8dacfc9d927bf944a4
Amirreza <kaffashan.amirreza1@gmail.com>
2023-10-19 16:33:20 +0330

    add ctrl+L

--- xv6-public-master/console.c
+++ xv6-public-master/console.c
@@ -287,6 +287,18 @@ void consoleintr(int (*getc)(void))
         // print_cursor_right_hand();
       }
       break;
+    case C('L'):
+      for (int i = 0; i < 25 * 80; i++)
+        crt[i] = ' ' | 0x0700; // Clear the screen
+      outb(CRTPORT, 14);
+      outb(CRTPORT + 1, 0);
+      outb(CRTPORT, 15);
+      outb(CRTPORT + 1, 1);
+      input.e = input.w = input.r; // Reset input buffer indices
+      input.line_ahead_size = 0;
+      crt[0] = '$' | 0x0700 ;
+      break;
+
     case C('H'):
     case '\x7f': // Backspace
       if (input.e != input.w)
