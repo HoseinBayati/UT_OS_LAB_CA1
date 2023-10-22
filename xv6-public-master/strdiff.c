7e9969e6b95647ec3c3bacb890668ce027076ff5
AmirrezaKn <kaffashan.amirreza1@gmail.com>
2023-10-22 19:52:56 +0330

    adding strdiff file to the repo

--- xv6-public-master/strdiff.c
+++ xv6-public-master/strdiff.c
@@ -1,48 +1,48 @@
 #include "types.h"
+#include "stat.h"
 #include "user.h"
-
-int main(int argc, char *argv[])
-{
-    for(uint i = 1 ; i < argc ; i++){
-        printf(1, "%s ", argv[i]);
+#include "fcntl.h"
+void compare_strings(int fd, const char* str1, const char* str2){ 
+    int len1 = strlen(str1);
+    int len2 = strlen(str2);
+    int max_len;
+    if(len1 > len2){
+        max_len = len1;
     }
-    printf(1, "\n");
-
-    printf(1, "%d \n", strcmp(argv[1], argv[2]));
-
-
-
-    // char str1[15] = "";
-    // // int str1_len = 0;
-    // char str2[15] = "";
-    // // int str2_len = 0;
-    // int i = 0;
-    // while (1)
-    // {
-    //     char curr_char[1];
-
-    //     read(0, curr_char, sizeof(char));
-    //     if (curr_char[0] == ' ')
-    //         break;
-
-    //     str1[i] = curr_char[0];
-    //     i++;
-    // }
-    // // str1_len = i + 1;
-
-    // i = 0;
-    // while (1)
-    // {
-    //     char curr_char[1];
-
-    //     read(0, curr_char, sizeof(char));
-    //     if (curr_char[0] == '\n' || curr_char[0] == ' ')
-    //         break;
-
-    //     str2[i] = curr_char[0];
-    //     i++;
-    // }
-    // str2_len = i + 1;
-
-    // printf(1, "%d \n", strcmp(str1, str2));
+    else{
+        max_len = len2;
+    }
+    for(int i = 0; i < max_len; i++){
+        int tmp1, tmp2;
+        if(i < len1){
+            tmp1 = ((int)str1[i] - 64) % 32;
+        }
+        else{
+            tmp1 = -1;
+        }
+        if(i < len2){
+            tmp2 = ((int)str2[i] - 64) % 32;
+        }
+        else{
+            tmp2 = -1;
+        }
+        if(tmp1 < tmp2){
+            printf(fd, "%d", 1);
+        }
+        else{
+            printf(fd, "%d", 0);
+        }   
+    }
+    printf(fd, "\n");
+    return;
+}
+int main(int argc, char *argv[]) {
+    unlink("strdiff_result.txt");
+    int fd = open("strdiff_result.txt", O_CREATE | O_WRONLY);
+    if(fd < 0){
+        printf(1, "Can;t create the file");
+    }
+    compare_strings(fd, argv[1], argv[2]);
+    close(fd);
+    exit();
 }
